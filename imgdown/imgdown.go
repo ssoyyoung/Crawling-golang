7bd24a628a707e0e1c39982651c4e164c1e72bbd
ssoyyoung <ssoyyoung.p@gmail.com>
2020-08-06 17:23:17 +0900

    Using goroutine! for speed up

--- imgdown/imgdown.go
+++ imgdown/imgdown.go
@@ -4,15 +4,50 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"sync"
+	"time"
 
+	parsing "github.com/ssoyyoung.p/Crawling-golang/parsing"
 	"github.com/ssoyyoung.p/Crawling-golang/utils"
 )
 
+// FullProcess func
+func FullProcess(cateURL string, c1 chan<- string) {
+
+	baseURL := "https://benito.co.kr"
+	debug := false
+	fmt.Println("Starting Crawling", cateURL)
+	crawlLink := baseURL + cateURL
+	cateN := utils.SplitData(crawlLink, "=", 1)
+	utils.CreateDir(cateN)
+
+	start := time.Now()
+	resultList := parsing.GOgetProductPage(crawlLink)
+	fmt.Println(crawlLink, ">>", len(resultList))
+
+	for i, result := range resultList {
+		if debug && i >= 1 {
+			break
+		}
+		productNum := utils.SplitData(result.URL, "/", 6)
+		productPath := cateN + "/" + productNum
+		utils.CreateDir(productPath)
+		ImgDownloading(result.ImgList, productPath)
+	}
+	elapsed := time.Since(start)
+	log.Printf("Binomial took %s", elapsed)
+
+	c1 <- cateURL
+
+}
+
 // ImgDownloading func
 func ImgDownloading(imgList []string, productPath string) {
+	var wg sync.WaitGroup
+
 	fileName := "imgFiles/error.txt"
 	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644)
 	utils.CheckErr(err)
@@ -42,7 +77,7 @@ func ImgDownloading(imgList []string, productPath string) {
 
 				if err != nil {
 					fmt.Println("Error while Downloading", url, "-", err)
-					if _, err := file.WriteString(url + "\n"); err != nil {
+					if _, err := file.WriteString(productPath + ", " + url + "\n"); err != nil {
 						fmt.Println(">>>Writing error!", url)
 					}
 				}
@@ -53,34 +88,3 @@ func ImgDownloading(imgList []string, productPath string) {
 	wg.Wait()
 	fmt.Println("[DONE] downloaded img", productPath, ">>", len(imgList))
 }
-
-var wg sync.WaitGroup
-
-// func downloading(errorFile *os.File, url string, imgName string) {
-// 	wg.Add(1)
-// 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-// 	res, err := http.Get(url)
-
-// 	if err != nil {
-// 		fmt.Println("http get error: ", url, "-", err)
-// 		if _, err := errorFile.WriteString(url + "\n"); err != nil {
-// 			fmt.Println(">>>Writing error!", url)
-// 		}
-// 	} else {
-// 		output, err := os.Create(imgName)
-// 		if err != nil {
-// 			fmt.Println("Error while createing", imgName, "-", err)
-// 		}
-// 		_, err = io.Copy(output, res.Body)
-// 		output.Close()
-// 		res.Body.Close()
-
-// 		if err != nil {
-// 			fmt.Println("Error while Downloading", url, "-", err)
-// 			if _, err := errorFile.WriteString(url + "\n"); err != nil {
-// 				fmt.Println(">>>Writing error!", url)
-// 			}
-// 		}
-// 	}
-// 	wg.Done()
-// }
--- main.go
+++ main.go
@@ -2,57 +2,27 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"time"
 
 	img "github.com/ssoyyoung.p/Crawling-golang/imgdown"
 	parsing "github.com/ssoyyoung.p/Crawling-golang/parsing"
-	utils "github.com/ssoyyoung.p/Crawling-golang/utils"
 )
 
 func main() {
 
-	debug := false
 	baseURL := "https://benito.co.kr"
 	categoryURL := parsing.GetCategory(baseURL)
 
-	for idx, cateURL := range categoryURL {
-		fmt.Println(cateURL)
-		crawlLink := baseURL + cateURL
-		cateN := utils.SplitData(crawlLink, "=", 1)
-		utils.CreateDir(cateN)
+	var resultList []string
+	c1 := make(chan string)
 
-		if debug && idx >= 1 {
-			break
-		}
-
-		start := time.Now()
-		resultList := parsing.GOgetProductPage(crawlLink)
-		fmt.Println(crawlLink, ">>", len(resultList))
-
-		for i, result := range resultList {
-			if debug && i >= 1 {
-				break
-			}
-			productNum := utils.SplitData(result.URL, "/", 6)
-			productPath := cateN + "/" + productNum
-			utils.CreateDir(productPath)
-			img.ImgDownloading(result.ImgList, productPath)
-		}
-		elapsed := time.Since(start)
-		log.Printf("Binomial took %s", elapsed)
+	for _, cateURL := range categoryURL {
+		go img.FullProcess(cateURL, c1)
 	}
-}
 
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
+	for i := 0; i < len(categoryURL); i++ {
+		result := <-c1
+		fmt.Println("[DONE] crawling category", result)
+		resultList = append(resultList, result)
 
-func checkCode(res *http.Response) {
-	if res.StatusCode != 200 {
-		log.Fatalln("Status code err: %d %s", res.StatusCode, res.Status)
 	}
 }
